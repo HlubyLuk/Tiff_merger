9a2c647dcffbef15847e4ac6fd24178bad6bb3b8
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2021-07-16 09:59:26 +0200

    Chage tiff library.

--- src/tiffmerger/main.py
+++ src/tiffmerger/main.py
@@ -5,37 +5,22 @@ Created on Mar 8, 2019
 
 @author: hlubyluk
 '''
-from os import path
-from sys import argv
+from sys import argv, path
+from pathlib import Path
 
-from libtiff import TIFF
-from libtiff import TiffFile
+path.insert(0, '')
+
+from libs.tifffile import *
 
 TIFF_EXT = ".tif"
 
 if __name__ == '__main__':
-    # `w8` mean init writable bigtiff file.
-    output = TIFF.open(path.expanduser(argv[-1]), "w8")
-
-    argv_paths = map(lambda x: path.expanduser(x), argv[1:-1])
-
-    for path_file in argv_paths:
-        tiff_file = TiffFile(path_file)
-
-        print("==========================================================")
-        print(tiff_file.get_info())
-        print("==========================================================")
-
-        total = tiff_file.get_depth()
-
-        for idx, plane in enumerate(tiff_file.get_tiff_array().planes):
-            print("{} index {} from {}".format(path_file, idx, total))
-
-            img_data = plane.get_image()
-            output.write_image(img_data)
-
-        tiff_file.close()
-
-    output.close()
-
-    print("Done")
+    with TiffWriter(Path(argv[-1]).expanduser(), bigtiff=True) as output:
+        argv_paths = map(lambda x: Path(x).expanduser(), argv[1:-1])
+        for path_file in argv_paths:
+            # print(path_file)
+            with TiffFile(path_file) as input:
+                for page in input.pages:
+                    data = page.asarray()
+                    # print(data)
+                    output.write(data)
