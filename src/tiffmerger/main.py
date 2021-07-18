92d68655c16c433bf37d119b5e819b3c31a7dd18
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2021-07-18 13:24:29 +0200

    Print input arguments.

--- src/tiffmerger/main.py
+++ src/tiffmerger/main.py
@@ -8,19 +8,18 @@ Created on Mar 8, 2019
 from sys import argv, path
 from pathlib import Path
 
-path.insert(0, '')
-
 from libs.tifffile import *
 
 TIFF_EXT = ".tif"
 
 if __name__ == '__main__':
+    print("Join files [{}], into {}".format(
+        ", ".join(map(lambda x: str(Path(x).expanduser()), argv[1:-1])),
+        argv[-1]))
     with TiffWriter(Path(argv[-1]).expanduser(), bigtiff=True) as output:
         argv_paths = map(lambda x: Path(x).expanduser(), argv[1:-1])
         for path_file in argv_paths:
-            # print(path_file)
             with TiffFile(path_file) as input:
                 for page in input.pages:
                     data = page.asarray()
-                    # print(data)
                     output.write(data)
